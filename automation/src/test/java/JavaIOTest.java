8745281c7f1d69e231369cbc03fa36057b825e92
Oscar <castroxoscar@gmail.com>
2021-12-23 10:58:01 -0700

    Add project C test

--- automation/src/test/java/JavaIOTest.java
+++ automation/src/test/java/JavaIOTest.java
@@ -3,23 +3,19 @@ import static org.testng.Assert.assertTrue;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Stream;
-
 import org.testng.annotations.Test;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
+import framework.Basse;
 import framework.ConfigurationProvider;
 
 public class JavaIOTest {
 	@Test
-	public void canGtBrowserType() {
+	public void canGetBrowserType() {
 		ConfigurationProvider provider = new ConfigurationProvider();
 		try {
 			HashMap<String, String> properties = provider.getPropertiesFromResourceFile("config.properties");
@@ -61,4 +57,31 @@ public class JavaIOTest {
 		  e.printStackTrace();
 	  }
   	}
+  
+  @Test
+  public void canGetBasseObject() {
+	  try {
+		  CSVReader bassesFile = new CSVReader(new FileReader(getClass().getResource("basses.csv").getPath()));
+		  bassesFile.skip(1);
+		  String[] row = null;
+		  ArrayList<Basse> basses = new ArrayList<Basse>();
+
+		  while((row = bassesFile.readNext()) != null) {
+			  basses.add(new Basse(row[0], row[1], Integer.parseInt(row[2])));
+		  }
+		  
+		  for(Basse b: basses) {
+			  System.out.println("Make: " + b.getMake());
+			  System.out.println("Model: " + b.getModel());
+			  System.out.println("String Count: " + b.getStringCount());
+			  System.out.println();
+		  }
+		  
+		  assertTrue(basses.getClass() != null);
+	  } catch (CsvValidationException e) {
+		  e.printStackTrace();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
+  }
 }
