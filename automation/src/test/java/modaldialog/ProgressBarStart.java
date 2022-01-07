d734b6fe1405bd1437c8d49d35779ce7a69027d5
tealek.alnutt <tealek.alnutt@teales-mbp>
2022-01-06 21:18:52 -0500

    Added Progress Bar Tests (Teale)

---
+++ automation/src/test/java/modaldialog/ProgressBarStart.java
@@ -0,0 +1,28 @@
+package modaldialog;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProgressBarStart {
+	private WebElement startButton;
+	private WebDriver driver;
+	
+	public ProgressBarStart(WebDriver driver) {
+		this.driver=driver;
+		
+		 startButton = driver.findElement(By.xpath(".//button[@id='startStopButton']"));
+	}
+	public void start() {
+	
+		startButton.click();
+		WebDriverWait wait = new WebDriverWait(this.driver,30);
+		wait.until(ExpectedConditions.attributeToBe(By.xpath(".//div[@role='progressbar']"),"aria-valuenow" , "100"));
+
+	}
+	public void reset() {
+		driver.findElement(By.xpath(".//button[@id='resetButton']")).click();
+	}
+}
---
+++ automation/src/test/java/modaldialog/ProgressBarTests.java
@@ -0,0 +1,43 @@
+package modaldialog;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class ProgressBarTests {
+	
+	private WebDriver driver;
+  
+@Test
+  public void canClickStartandWait() {
+	ProgressBarStart progressBarStart = new ProgressBarStart(this.driver);
+	progressBarStart.start();  
+	progressBarStart.reset();
+	
+	Assert.assertEquals(driver.findElement(By.xpath(".//div[@role='progressbar']")).getText(),"0%");
+  }
+  @BeforeMethod
+  public void setup() {
+	  canLaunchWebDriver();
+  }
+  
+  @AfterMethod
+  public void cleanup() {
+	  this.driver.quit();
+  }
+	  public void canLaunchWebDriver() {
+		  String driverPath = "/Users/tealek.alnutt/Downloads/chromedriver";
+		  System.setProperty("webdriver.chrome.driver", driverPath);
+
+		  String url = "https://demoqa.com/progress-bar";
+		  driver = new ChromeDriver();
+
+		  driver.navigate().to(url);
+		  String currentURL = driver.getCurrentUrl();
+		  Assert.assertEquals(url, currentURL);
+	  }  
+}
