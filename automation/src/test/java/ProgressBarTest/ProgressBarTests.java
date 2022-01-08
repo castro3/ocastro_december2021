698f69361439c13c557cdd2dc5163a6054b47bec
tealek.alnutt <tealek.alnutt@teales-mbp>
2022-01-07 19:45:59 -0500

    Updated Progress Bar Tests (Teale)

--- automation/src/test/java/ProgressBarTest/ProgressBarTests.java
+++ automation/src/test/java/ProgressBarTest/ProgressBarTests.java
@@ -1,44 +1,60 @@
-package ProgressBarTest;
-
 import org.testng.annotations.Test;
 
-import org.testng.AssertJUnit;
+import modaldialog.ProgressBarTest.ProgressBarPage;
+
 import org.testng.annotations.BeforeMethod;
-import org.openqa.selenium.By;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 public class ProgressBarTests {
-	
+
 	private WebDriver driver;
-  
-@Test
-  public void canClickStartandWait() {
-	ProgressBarStart progressBarStart = new ProgressBarStart(this.driver);
-	progressBarStart.start();  
-	progressBarStart.reset();
-	
-	AssertJUnit.assertEquals(driver.findElement(By.xpath(".//div[@role='progressbar']")).getText(),"0%");
-  }
-  @BeforeMethod
-  public void setup() {
-	  canLaunchWebDriver();
-  }
-  
-  @AfterMethod
-  public void cleanup() {
-	  this.driver.quit();
-  }
-	  public void canLaunchWebDriver() {
-		  String driverPath = "/Users/tealek.alnutt/Downloads/chromedriver";
-		  System.setProperty("webdriver.chrome.driver", driverPath);
 
-		  String url = "https://demoqa.com/progress-bar";
-		  driver = new ChromeDriver();
+	@Test
+	public void canClickStartandStop() {
+		int input = 9;
+		ProgressBarPage progressBar = new ProgressBarPage(this.driver);
+		progressBar.navigate();
+		progressBar.start();  
+		progressBar.stop(input); 
 
-		  driver.navigate().to(url);
-		  String currentURL = driver.getCurrentUrl();
-		  AssertJUnit.assertEquals(url, currentURL);
-	  }  
+		Assert.assertNotEquals(progressBar.getValue(), 0);
+	} 
+	@Test
+	public void canClickStartandReset() {
+		ProgressBarPage progressBar = new ProgressBarPage(this.driver);
+		progressBar.navigate();
+		progressBar.start();  
+		progressBar.reset();
+
+		Assert.assertEquals(progressBar.getValue(), 0);
+	}
+	@BeforeMethod
+	public void setup() {
+		canLaunchWebDriver();
+	}
+
+	@AfterMethod
+	public void cleanup() {
+		this.driver.quit();
+	}
+	public void canLaunchWebDriver() {
+		Path resourceDirectory = Paths.get("src","test","resources");
+		String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+		Path driverFile = Paths.get(absolutePath, "chromedriver.exe");
+
+		System.setProperty("webdriver.chrome.driver", driverFile.toFile().getAbsolutePath());
+
+		this.driver = new ChromeDriver(); 
+
+		long pageLoadTimeout = 3;
+		this.driver.manage().timeouts().implicitlyWait(pageLoadTimeout , TimeUnit.SECONDS);
+	}  
 }
